3457c9ce3cd549296697ca968cd098bd2199059d
cmplx-xyttmt <archimedesisaac@gmail.com>
2019-07-26 12:07:32 +0300

    Setup navigation and databinding

--- app/src/main/java/com/andela/cityweatherforecast/MainActivity.kt
+++ app/src/main/java/com/andela/cityweatherforecast/MainActivity.kt
@@ -2,11 +2,16 @@ package com.andela.cityweatherforecast
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.andela.cityweatherforecast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        val navController = this.findNavController(R.id.myNavHostFragment)
     }
 }
