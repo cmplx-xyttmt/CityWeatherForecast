2a15485f5b6f6cf8b0b010bec0dc55a8a361b58d
cmplx-xyttmt <archimedesisaac@gmail.com>
2019-07-26 13:05:21 +0300

    Setup a basic home-screen

---
+++ app/src/main/java/com/andela/cityweatherforecast/homescreen/HomeScreenFragment.kt
@@ -0,0 +1,20 @@
+package com.andela.cityweatherforecast.homescreen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.andela.cityweatherforecast.R
+import com.andela.cityweatherforecast.databinding.FragmentHomeScreenBinding
+
+class HomeScreenFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        val binding = DataBindingUtil.inflate<FragmentHomeScreenBinding>(inflater, R.layout.fragment_home_screen, container, false)
+
+        return binding.root
+    }
+}
